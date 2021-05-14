30cd96f9242c08b57d54493be563aab67bb3f19f
Ken <kennyish1173@gmail.com>
2021-05-14 19:23:53 +0900

    Added test cases for invitation window

--- test/ui-objects/settingsWindow.js
+++ test/ui-objects/settingsWindow.js
@@ -1,5 +1,11 @@
 module.exports = {
-    windowIndex : 1,
-    accountSettingsIcon: '#app > div > div > div.setting_container > div.layout.row.wrap.justify-center.align-center > div:nth-child(2)' //old
-    //accountSettingsIcon: '#app > div > div > div.setting_container > div.layout.row.wrap.justify-center.align-center > div:nth-child(2) > button > div'
+    windowIndex: 1, //when accessed from cog wheel icon
+    windowIndex_2: 2, //when accessed from Invite Members button
+    closeButton: '#app > div > div.header > div.header_close > button',
+    accountSettingsIcon: '#app > div > div > div.setting_container > div.layout.row.wrap.justify-center.align-center > div:nth-child(2)', //old
+    //accountSettingsIcon: '#app > div > div > div.setting_container > div.layout.row.wrap.justify-center.align-center > div:nth-child(2) > button > div',
+
+    //Inivitation List
+    memberInvitationTitle: '#app > div > div.appli_setting_container > div > div:nth-child(5) > div > div:nth-child(1) > h1',
+    memberInvitationTitleText: 'メンバー招待'
 }
