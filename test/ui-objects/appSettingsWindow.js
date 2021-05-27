6a7077c36ceec97df651b1353ff0c12c51bf046d
Ken <kennyish1173@gmail.com>
2021-05-27 15:03:48 +0900

    Added App Setting Test Case

---
+++ test/ui-objects/appSettingsWindow.js
@@ -0,0 +1,28 @@
+module.exports = {
+    windowIndex: 2,
+    currentSelection: '#app > div > div.appli_setting_container > ul > li.app_setting_menu.active',
+    general: '#app > div > div.appli_setting_container > ul > li:nth-child(1)',
+    notification: '#app > div > div.appli_setting_container > ul > li:nth-child(2)',
+    audio: '#app > div > div.appli_setting_container > ul > li:nth-child(3)',
+    calendar: '#app > div > div.appli_setting_container > ul > li:nth-child(4)',
+    autoStatus: '#app > div > div.appli_setting_container > ul > li:nth-child(5)',
+    memberList: '#app > div > div.appli_setting_container > ul > li:nth-child(6)',
+    payment: '#app > div > div.appli_setting_container > ul > li:nth-child(7)',
+    resetSettings: '#app > div > div.appli_setting_container > ul > li:nth-child(8)',
+
+    //General Settings
+    shortcutSetting: '#app > div > div.appli_setting_container > div > div:nth-child(1) > div > div:nth-child(3) > div > div.input-group__input > div.input-group__selections > div',
+    shortcutSettingDropDown: '#app > div > div.appli_setting_container > div > div:nth-child(1) > div > div:nth-child(3) > div > div.input-group__input > div.input-group__selections',
+    shortcut_rightShift: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(1) > a > div > div',
+    shortcut_leftShift: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(2) > a > div > div',
+    shortcut_rightControl: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(3) > a > div > div',
+    shortcut_leftControl: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(4) > a > div > div',
+    shortcut_shiftControl: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(5) > a > div > div',
+    shortcut_windowsAlt: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(6) > a > div > div',
+    shortcut_windowsShift: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(7) > a > div > div',
+    languageSetting: '#app > div > div.appli_setting_container > div > div:nth-child(1) > div > div:nth-child(4) > div > div.input-group__input > div.input-group__selections > div',
+    languageSettingDropDown: '#app > div > div.appli_setting_container > div > div:nth-child(1) > div > div:nth-child(4) > div > div.input-group__input > div.input-group__selections',
+    language_jap: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(1) > a > div > div',
+    language_eng: '#app > div.menu__content.menu__content--select.menuable__content__active > div > div > div:nth-child(2) > a > div > div'
+    
+}
